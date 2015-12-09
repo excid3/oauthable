5c9e5565316d8ac8b645ef36d0df3cccd255f537
Chris Oliver <excid3@gmail.com>
2015-12-08 23:26:52 -0600

    Move providers to a constant to make precompiling work without hitting the database

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if ActiveRecord::Base.connection.table_exists? 'providers'
-    Provider.find_each do |p|
-      provider p.omniauth_name, setup: true
-    end
+  PROVIDERS.each do |omniauth_name, _, _, _, _|
+    provider omniauth_name, setup: true
   end
 end
 
