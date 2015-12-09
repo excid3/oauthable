f27e97679abf975ec4ec1b955d5aa4549f5e4f45
Chris Oliver <excid3@gmail.com>
2015-12-09 15:51:05 -0600

    Scope projects index to user's projects only

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = current_user.projects
   end
 
   # GET /projects/1
